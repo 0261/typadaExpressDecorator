49e3e78e1b4e8ab75d30f08d22b760d8b4de588a
0261 <bada013@naver.com>
2019-04-11 18:27:54 +0900

    return proxyInstance

--- src/factory.ts
+++ src/factory.ts
@@ -5,24 +5,31 @@ import { ApplicationMethodMetadata, ProxyInstance } from './interface';
 
 
 class TypadaFactory {
-    private readonly app:Express.Application = Express();
 
-    createInstance(applicationMiddleware: Array<Express.RequestHandler> = []) {
+    private readonly app: Express.Application = Express();
+
+    createInstance(applicationMiddleware: Array<Express.RequestHandler> = []): ProxyInstance {
         
         const controllers = Reflect.getMetadata(META_DATA.application, Reflect) as Array<ApplicationMethodMetadata>;
-        const attachedMiddleware = this.attachedMiddleware(controllers, applicationMiddleware);
 
-        return this.createProxyInstance();
+        const proxyInstance = this.createProxyInstance();
+
+        this.attachedMiddleware(controllers, applicationMiddleware);
+
+        return proxyInstance;
     }
     // attachControllerLevelMiddleware
     attachedMiddleware(controllers: Array<ApplicationMethodMetadata>, applicationMiddleware: Array<Express.RequestHandler> = []): boolean {
         try {
+            
             if(!controllers) return false;
+
             controllers.map(controller => {
                 const {basePath, router, middlewares } = controller;
                 const totalMiddlewares = applicationMiddleware.length < 1 ? middlewares : middlewares.concat(applicationMiddleware);
                 this.app.use(basePath, ...totalMiddlewares, router);
             });
+
             return true;
         } catch (error) {
             console.log(`attachControllerLevelMiddleware`, error);
@@ -35,14 +42,13 @@ class TypadaFactory {
         try {
             const handler = {
                 get: (obj, prop) => {
-                    
                     if(META_DATA.AVAILABLE_EXPRESS_PROPERTY.includes(typeof prop === 'symbol' ? 'do not' : prop)){
                         return obj[prop]
                     }
                     return `Invalid Property: ${prop} property`;
                 }
             }
-            const proxyInstance = new Proxy(this.app, handler) as ProxyInstance;
+            const proxyInstance = new Proxy(Express(), handler) as ProxyInstance;
             return proxyInstance;
         } catch (error) {
             console.log(error);
